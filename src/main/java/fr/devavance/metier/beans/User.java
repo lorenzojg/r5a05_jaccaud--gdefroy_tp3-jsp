7564c3fb465c6a751ed64662534e3c3aa37d7520
Lorenzo JACCAUD--GODEFROY <lorenzojg@hotmail.fr>
2025-11-10 09:59:24 +0100

    update User

--- src/main/java/fr/devavance/metier/beans/User.java
+++ src/main/java/fr/devavance/metier/beans/User.java
@@ -1,10 +1,12 @@
 package fr.devavance.metier.beans;
 
+import java.io.Serializable;
+
 /**
  *
  * @author bouchaib.lemaire
  */
-public class User {
+public class User implements Serializable {
 
     // properties
     private String userName = "";  // Nom d'utilisateur
@@ -12,6 +14,13 @@ public class User {
     private String profil = "";  // Profil de l'utilisateur "admin" ou "user"
     private Boolean auth = false; // Important : déconnecté par défaut
 
+    public User(){}
+    
+    public String getUserName(){return this.userName;}
+    
+    public void setUserName(String n_userName){this.userName = n_userName;}
+    
+    
     
 
 
